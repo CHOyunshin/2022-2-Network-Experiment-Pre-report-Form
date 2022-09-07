ef90e0d1bb09a04e5b4610feb2a203f4b552cee3
ysCHOI <815choyunsin@yonsei.ac.kr>
2022-09-07 10:55:37 +0900

    pre week02 내용 입력

--- code/adder.v
+++
@@ -1,9 +0,0 @@
-module addertwobit(
-
-    input [1:0] input_a,
-    output [3:0] result
-    );
-    
-        assign result = input_a + 3'b111;
-        
-endmodule
--- code/and.v
+++
@@ -1,9 +0,0 @@
-module logic_gate(
-input input_a,
-input input_b,
-output result
-    );
-    
-    assign result = input_a & input_b;
-    
-endmodule
